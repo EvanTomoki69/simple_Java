cc70cfdba2eca4ecf7a80d522f86161d680143fe
EvanTomoki69 <106935385+EvanTomoki69@users.noreply.github.com>
2022-06-19 22:30:50 -0500

    Add files via upload
    Reto 6 de la UVEG Fundamentos de programación
    En el código Java se integraron los siguientes elementos:
    - Estructura de la clase, llamada Sumanumeros
    - Dos valores que al ser sumados devuelvan la siguiente cadena: El resultado es = (a esto concatenar el resultado de la operación)
    - Importación del Scanner reader

---
+++ sumanumeros/Sumanumeros.java
@@ -0,0 +1,26 @@
+
+
+package evantomokij.sumanumeros;
+
+import java.util.Scanner;
+
+
+public class Sumanumeros {
+
+      public static void main(String[] args) {
+       Scanner reader = new Scanner(System.in);
+       int numero1 = 0;
+       int numero2 = 0;
+       
+       System.out.println("Introduce el primer número:");
+       numero1 = reader.nextInt();
+       
+       System.out.println("Introduce el segundo número:");
+       numero2 = reader.nextInt();
+       
+        int resultado = numero1+numero2;
+        
+        System.out.println("La suma es " + numero1 + "+" + numero2 + "=" + resultado);
+
+    }
+}
