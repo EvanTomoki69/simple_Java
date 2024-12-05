11575d1069940446821c764dc88277e86280a48e
EvanTomoki69 <evantomoki@gmail.com>
2024-12-04 22:33:30 -0600

    new folder with Java excercises added

---
+++ javaSamples/etgLearn/objects/M3.java
@@ -0,0 +1,15 @@
+//Class cretion using "new keyword" method
+public class M3 {
+    //Declaring and intializing string
+    //Custom input string
+     String car = "BMW M3 GTR";
+
+     public static void main(String[] args){
+        //Generic implementation creating the class inside (main)
+        M3 carCreated = new M3();
+
+        System.out.println(carCreated.car);
+     }
+
+ }
+ 
